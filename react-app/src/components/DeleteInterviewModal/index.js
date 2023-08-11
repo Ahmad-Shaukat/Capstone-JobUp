9c191bd2f1c1911f44be0b1e548a094bae1e204d
ahmad-shaukat <ahmadshaukat@yahoo.com>
2023-08-10 22:53:45 -0400

    added css to delete interview modal

--- react-app/src/components/DeleteInterviewModal/index.js
+++ react-app/src/components/DeleteInterviewModal/index.js
@@ -3,11 +3,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { deleteInterviewThunk } from "../../store/interview";
 import { useModal } from "../../context/Modal";
+import './app.css'
 
-function DeleteInterview({interview}) {
+function DeleteInterview({ interview }) {
     const dispatch = useDispatch()
     const history = useHistory()
-    const {closeModal} = useModal()
+    const { closeModal } = useModal()
 
 
     const handleDelete = async (e) => {
@@ -17,10 +18,22 @@ function DeleteInterview({interview}) {
     }
 
     return <>
-    <h4>Delete Interview?</h4>
-    <p>are you sure you want to delete this interview</p>
-    <button type="submit" onClick={handleDelete}>Delete</button>
-    
+        <div className="del-int-main-cont">
+            <div className="del-int-heading">
+                <p>Delete Interview</p>
+            </div>
+            <div className="del-int-text">
+                <p>Are you sure you want to delete this interview ?</p>
+
+            </div>
+            <div className="del-int-btn">
+                <button type="submit" onClick={handleDelete} className="del-int-del-btn">Delete</button>
+                <button className="del-int-can-btn" onClick={() => closeModal()}>Cancel</button>
+                
+
+            </div>
+        </div>
+
     </>
 
 }
