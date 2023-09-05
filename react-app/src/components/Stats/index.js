fb9204f8a616119051dcadcff17e5a29ae271893
ahmad-shaukat <ahmadshaukat@yahoo.com>
2023-09-04 22:36:56 -0400

    finished functionality of adding graphs

--- react-app/src/components/Stats/index.js
+++ react-app/src/components/Stats/index.js
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
 import OpenModalButton from "../OpenModalButton";
-import interviewsReducer, { getAllInterviewsThunk } from "../../store/interview";
+import interviewsReducer, {
+  getAllInterviewsThunk,
+} from "../../store/interview";
 import { FaSuitcaseRolling, FaCalendarCheck, FaBug } from "react-icons/fa";
 import "./app.css";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
@@ -20,33 +23,40 @@ function Stats() {
   const history = useHistory();
   let state = useSelector((store) => store);
   let user = useSelector((store) => store.session["user"]);
-//   const events = []
+  const [showInterviews, setShowInterviews] = useState(false)
+  const [showDashboard, setShowDashboard] = useState(true)
+
+  //   const events = []
 
   // console.log (state, '--------------------this is state')
   let interviews = useSelector((store) => store.interview);
   interviews = Object.values(interviews);
 
+  // Format all the interviews to have new format
 
-//   using map to get all the events
-  let events = interviews.map((interview) => ({
+  const formatedInterview = interviews.map((interview) => {
+    let initalDateString = interview.date;
+    let initialDate = new Date(initalDateString.replace(/ GMT$/, ""));
+    // we are using UTC so that time is not effected by different zones or day light saving
+    let year = String(initialDate.getUTCFullYear());
+    //  we are adding month because time objects return a zero based montths. and we are adding 0 as padding so instead of getting just 2 for februry we will get 02.
+    let month = String(initialDate.getUTCMonth() + 1).padStart(2, "0");
+    let day = String(initialDate.getUTCDate()).padStart("2", "0");
+    let date = `${year}-${month}-${day}`;
+    return { ...interview, date: date };
+  });
+  //   console.log (formatedInterview, '---------------------')
+  console.log(interviews, "-----------------yellah");
+  console.log(formatedInterview, "---------------formated");
+
+  //   using map to make events array
+  let events = formatedInterview.map((interview) => ({
     title: `${interview.company}: ${interview.status}`,
-    date: interview.date
-  } ))
+    date: interview.date,
+  }));
 
-//   format the date for all the calander using the date object
+  //   format the date for events using the date object
 
-const formatEventDates = (events) => {
-    let monthObject = {}
-    for (let i = 0; i<events.length; i++) {
-        let initalDateString = events[i].date
-        let initialDate = new Date (initalDateString.replace(/ GMT$/, ''))
-        console.log (initialDate, '-----------------------')
-    }
-}
-formatEventDates(events)
-  console.log(interviews, "-----------these are all interviews");
-  console.log (events, 'these are all the events')
-  // let pendingInterviews = interviews.filter(())
   const pendingInterviews = Object.values(interviews).filter(
     (interview) => interview.status === "Pending"
   );
@@ -56,6 +66,25 @@ formatEventDates(events)
   const declinedInterviews = Object.values(interviews).filter(
     (interview) => interview.status === "Declined"
   );
+  //   handleDateClicks function checks for inteveiws that are on this
+  const handleDateClick = (args) => {
+    // use array.some method to see if any interviews exsists on that date
+    let hasInterviews = events.some((event) => {
+      return event.date == args.dateStr;
+    });
+    if (hasInterviews) {
+        setShowInterviews(true)
+        setShowDashboard(false)
+    } else {
+        window.alert('Nothing to show')
+    }
+
+  };
+// this functions controls the dashboard once user opens up the interviews through calander
+  const closeInterviewsHandle = () => {
+    setShowInterviews(false)
+    setShowDashboard(true)
+  }
   // console.log (pendingInterviews.length, 'these are pending interivew --------------')
   // console.log (scheduledInterviews.length, 'these are scheduled interviews')
   // console.log (declinedInterviews.length, 'these are declined interviews')
@@ -119,27 +148,42 @@ formatEventDates(events)
             </div>
           </div>
         </div>
-        <div className="calander-container">
-          <button className="add-event-btn" id="tests">
-            <MdAdd />
-          </button>
-          <div className="calander">
-            <FullCalendar
-              id="calander2"
-              className="my-calendar" // Add a className here
-              plugins={[dayGridPlugin, timeGridPlugins, interactionPlugin]}
-              initialView="dayGridMonth"
-              headerToolbar={{
-                start: "today prev,next",
-                center: "title",
-                end: "dayGridMonth,timeGridWeek",
-              }}
-              events={[
-                { title: "event 1", date: "2019-04-01", backgroundColor:'red' },
-                { title: "event 2", date: "2023-09-02" },
-                { title: "event 3", date: "2023-09-02" },
-              ]}
-            />
+        <div className="main-stats-btm">
+          <div className="calander-container">
+            <button className="add-event-btn" id="tests">
+              <MdAdd />
+            </button>
+            <div className="calander">
+              <FullCalendar
+                id="calander2"
+                className="my-calendar" // Add a className here
+                plugins={[dayGridPlugin, timeGridPlugins, interactionPlugin]}
+                dateClick={handleDateClick}
+                initialView="dayGridMonth"
+                headerToolbar={{
+                  start: "today prev,next",
+                  center: "title",
+                  end: "dayGridMonth,timeGridWeek",
+                }}
+                events={events}
+              />
+            </div>
+          </div>
+          <div className="right-side-main">
+            <div className="stats__sub">
+              {showInterviews ? (
+                <div className="showInterviews">
+                  <h1>showInterviews</h1>
+                  <button onClick={closeInterviewsHandle}>Close</button>
+                </div>
+              ) : null}
+
+              {showDashboard ? (
+                <div className="showDashboard">
+                    <h1>show Dashboard</h1>
+                </div>
+              ) : null}
+            </div>
           </div>
         </div>
       </main>
