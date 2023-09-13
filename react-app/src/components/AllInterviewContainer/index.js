96685b722cc335a6fdfbddb6f48c80f6d935cdab
ahmad-shaukat <ahmadshaukat@yahoo.com>
2023-09-12 22:00:05 -0400

    added interview status functionality

--- react-app/src/components/AllInterviewContainer/index.js
+++ react-app/src/components/AllInterviewContainer/index.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import OpenModalButton from "../OpenModalButton";
 import EditInterviewForm from "../EditInterviewForm";
-import { format } from "date-fns";
+import { format, set } from "date-fns";
 import DeleteInterview from "../DeleteInterviewModal";
 import { NavLink, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import "./app.css";
@@ -18,9 +18,27 @@ const AllInterviews = ({}) => {
   const [editInterview, setEditInterview] = useState({});
   const [showEdit, setShowEdit] = useState(false);
   let { interview } = useSelector((store) => store);
+  const [showInt, setShowInt] = useState(true);
+  const [searchPosition, setSearchPosition] = useState(false);
+  const [searchStatus, setSearchStatus] = useState(false);
+  const [searchCompany, setSearchCompany] = useState(false);
+  const [searchResult, setSearchResult] = useState([]);
+  const [showSearch, setShowSearch] = useState(false);
+
   const history = useHistory();
   interview = Object.values(interview);
   console.log(interview);
+  const StatusInterview = (status) => {
+    return interview.filter((interview) => interview.status);
+  };
+  const positionInterview = (position) => {
+    position = position.toString().toLowerCase(); // Convert to lowercase after ensuring it's a string
+    return interview.filter((interview) =>
+      interview.position.toLowerCase().includes(position)
+    );
+  };
+  // console.log(StatusInterview("Pending"), "---------------these are pending");
+
   useEffect(async () => dispatch(getAllInterviewsThunk()), [dispatch]);
 
   if (!user) {
@@ -41,17 +59,49 @@ const AllInterviews = ({}) => {
   };
 
   const afterSaving = () => {
-    setShowEdit(false)
-    setEditInterview({})
-    history.push('/interviews')
-  }
+    setShowEdit(false);
+    setEditInterview({});
+    history.push("/interviews");
+  };
+
+  const handleSearch = (e) => {
+    console.log(e.target.value.length);
+    if (e.target.value.length == 0) {
+      setShowInt(true);
+      setShowSearch(false);
+    } else {
+      console.log("in the else statement");
+      setSearchPosition(e.target.value);
+      console.log(searchPosition, "---------------search Position");
+      setSearchResult(positionInterview(searchPosition));
+
+      // console.log (poistionInterview(searchPosition, '-----------------result from search Position'))
+      setShowSearch(true);
+      setShowInt(false);
+      setSearchCompany(false);
+      setSearchStatus(false);
+    }
+    console.log(searchResult, "--------------searchResult");
+    console.log(showSearch);
+    // console.log (e.target.value.length)
+    // setShowInt(false)
+    // return (<>
+    // <h5>{e.target.vlaue}</h5>
+    // </>
+    // )
+  };
 
   // condition that opens up the form
   if (showEdit) {
     return (
       <>
         <div>
-          <EditInterviewForm id={editInterview.id}interview={editInterview} onCancel={cancelEdit} afterSaving={afterSaving}/>
+          <EditInterviewForm
+            id={editInterview.id}
+            interview={editInterview}
+            onCancel={cancelEdit}
+            afterSaving={afterSaving}
+          />
           {/* <button onClick={() => cancelEdit()}>Cancel</button> */}
         </div>
       </>
@@ -73,9 +123,197 @@ const AllInterviews = ({}) => {
       </>
     );
   }
+
   return (
     <>
-      <div className="allInt-container">
+      <div>
+        <h5>Search</h5>
+        <div>
+          <label>Position </label>
+          <input type="text" onChange={handleSearch}></input>
+        </div>
+        <div>
+          <label>Status </label>
+          <input type="text" onChange={handleSearch}></input>
+        </div>
+      </div>
+      {searchResult.length > 0 ? <div className="allInt-container">
+
+        {searchResult.map((interview) => (
+          <div key={interview.id} className="">
+            {showSearch ? (
+              <div className="allInt-column">
+                <div className="allInt-top-cont">
+                  <div className="allInt-com-letter">
+                    <p>{interview.company[0].toUpperCase()}</p>
+                  </div>
+                  <div>
+                    <p className="allInt-com-pos">{interview.position}</p>
+                    <p className="allInt-com-name">{interview.company}</p>
+                  </div>
+                </div>
+                <div className="allInt-btm-container">
+                  <div className="allInt-btm-left-cont">
+                    <div className="allInt-loc-type-cont">
+                      <p className="allInt-loc-text">
+                        <FaLocationArrow className="allInt-loc-logo" />
+                        {interview.location}
+                      </p>
+                      <p>
+                        <MdWork className="allInt-type-logo" />
+                        {interview.type}
+                      </p>
+                    </div>
+
+                    <div className="allInt-btm-bttns">
+                      <button
+                        className="allInt-edit-btn"
+                        onClick={() => showHandle(interview)}
+                      >
+                        Edit
+                      </button>
+
+                      {/* <button className='allInt-edit-btn'>
+                                <OpenModalButton
+                                    buttonText={'Edit'}
+                                    
+                                    modalComponent={
+                                        <EditInterviewForm
+                                            interview={interview} id={interview.id}
+                                        />
+                                    }
+                                />
+                            </button> */}
+
+                      <button className="allInt-del-btn">
+                        <OpenModalButton
+                          buttonText={"Delete"}
+                          modalComponent={
+                            <DeleteInterview interview={interview} />
+                          }
+                        />
+                      </button>
+
+                      <NavLink exact to={`/interview/${interview.id}/detail`}>
+                        <button className="allInt-btns-more">More</button>
+                      </NavLink>
+                    </div>
+                  </div>
+                  <div className="allInt-btm-right-container">
+                    <p className="allInt-date">
+                      <BsCalendar2Week className="allInt-int-logo" />
+                      {interview["date"].slice(0, 17)}
+                    </p>
+                    {interview.status === "Pending" && (
+                      <p className="allInt-Pending">{interview.status}</p>
+                    )}
+                    {interview.status === "Scheduled" && (
+                      <p className="allInt-Scheduled">{interview.status}</p>
+                    )}
+                    {interview.status === "Declined" && (
+                      <p className="allInt-Declined">{interview.status}</p>
+                    )}
+                    {interview.status === "Offered" && (
+                      <p className="allInt-Offered">{interview.status}</p>
+                    )}
+                  </div>
+                </div>
+              </div>
+            ) : null}
+          </div>
+        ))}
+      </div>: showSearch ? (
+        <div>No results found</div>
+      ) : null}
+      {showInt ? (
+        <div className="allInt-container">
+          {interview.map((interview) => {
+            // const interviewDate = new Date(interview.date)
+            // const formattedDate = format(interviewDate, 'EEEE, MMMM d, yyyy', { timeZone: 'GMT' });
+
+            return (
+              <div className="allInt-column">
+                <div className="allInt-top-cont">
+                  <div className="allInt-com-letter">
+                    <p>{interview.company[0].toUpperCase()}</p>
+                  </div>
+                  <div>
+                    <p className="allInt-com-pos">{interview.position}</p>
+                    <p className="allInt-com-name">{interview.company}</p>
+                  </div>
+                </div>
+                <div className="allInt-btm-container">
+                  <div className="allInt-btm-left-cont">
+                    <div className="allInt-loc-type-cont">
+                      <p className="allInt-loc-text">
+                        <FaLocationArrow className="allInt-loc-logo" />
+                        {interview.location}
+                      </p>
+                      <p>
+                        <MdWork className="allInt-type-logo" />
+                        {interview.type}
+                      </p>
+                    </div>
+
+                    <div className="allInt-btm-bttns">
+                      <button
+                        className="allInt-edit-btn"
+                        onClick={() => showHandle(interview)}
+                      >
+                        Edit
+                      </button>
+
+                      {/* <button className='allInt-edit-btn'>
+                                        <OpenModalButton
+                                            buttonText={'Edit'}
+                                            
+                                            modalComponent={
+                                                <EditInterviewForm
+                                                    interview={interview} id={interview.id}
+                                                />
+                                            }
+                                        />
+                                    </button> */}
+
+                      <button className="allInt-del-btn">
+                        <OpenModalButton
+                          buttonText={"Delete"}
+                          modalComponent={
+                            <DeleteInterview interview={interview} />
+                          }
+                        />
+                      </button>
+
+                      <NavLink exact to={`/interview/${interview.id}/detail`}>
+                        <button className="allInt-btns-more">More</button>
+                      </NavLink>
+                    </div>
+                  </div>
+                  <div className="allInt-btm-right-container">
+                    <p className="allInt-date">
+                      <BsCalendar2Week className="allInt-int-logo" />
+                      {interview["date"].slice(0, 17)}
+                    </p>
+                    {interview.status === "Pending" && (
+                      <p className="allInt-Pending">{interview.status}</p>
+                    )}
+                    {interview.status === "Scheduled" && (
+                      <p className="allInt-Scheduled">{interview.status}</p>
+                    )}
+                    {interview.status === "Declined" && (
+                      <p className="allInt-Declined">{interview.status}</p>
+                    )}
+                    {interview.status === "Offered" && (
+                      <p className="allInt-Offered">{interview.status}</p>
+                    )}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      ) : null}
+      {/* <div className="allInt-container">
         {interview.map((interview) => {
           // const interviewDate = new Date(interview.date)
           // const formattedDate = format(interviewDate, 'EEEE, MMMM d, yyyy', { timeZone: 'GMT' });
@@ -112,17 +350,6 @@ const AllInterviews = ({}) => {
                       Edit
                     </button>
 
-                    {/* <button className='allInt-edit-btn'>
-                                        <OpenModalButton
-                                            buttonText={'Edit'}
-                                            
-                                            modalComponent={
-                                                <EditInterviewForm
-                                                    interview={interview} id={interview.id}
-                                                />
-                                            }
-                                        />
-                                    </button> */}
 
                     <button className="allInt-del-btn">
                       <OpenModalButton
@@ -160,7 +387,7 @@ const AllInterviews = ({}) => {
             </div>
           );
         })}
-      </div>
+      </div> */}
     </>
   );
 };
