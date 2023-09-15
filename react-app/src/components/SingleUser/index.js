d94113012da0a8f0d9222bcfe6e0fc5e5105776b
ahmad-shaukat <ahmadshaukat@yahoo.com>
2023-09-15 19:08:42 -0400

    added styling to single user profile

--- react-app/src/components/SingleUser/index.js
+++ react-app/src/components/SingleUser/index.js
@@ -1,184 +1,177 @@
 import { useEffect } from "react";
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
 import { getAllInterviews, getAllInterviewsThunk } from "../../store/interview";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { getAllUsersInterviewThunk } from "../../store/allinterviews";
-import { FaLocationArrow } from 'react-icons/fa'
-import { MdWork } from 'react-icons/md'
-import { BsCalendar2Week } from 'react-icons/bs'
+import { FaLocationArrow } from "react-icons/fa";
+import { MdWork } from "react-icons/md";
+import { BsCalendar2Week } from "react-icons/bs";
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import './app.css'
+import "./app.css";
+import userImage from "../../utilities/user_image.jpg";
 
 const SingleUser = () => {
-    const history = useHistory()
-    const { id } = useParams()
-    const dispatch = useDispatch()
-    console.log(id, '-------------this is id')
+  const history = useHistory();
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  console.log(id, "-------------this is id");
 
-    let interviews = useSelector((store) => store?.allinterviews)
-    let user = useSelector((store) => store?.session['user'])
-    interviews = Object.values(interviews)
-    console.log(interviews, '-----------------these are all the interviews')
-    // console.log(interviews, '--------------------all the interviews')
-    const userInterviews = interviews.filter((interview) => Number(interview.userId) == id)
-    console.log(userInterviews, '--------------these belong to the users')
+  let interviews = useSelector((store) => store?.allinterviews);
+  let allUsers = useSelector((store) => store?.user);
 
-    // console.log (userInterviews, '------------these belong th the uses')
-
-    // const pendingInterviews = userInterviews  .filter((interview) => interview.status === 'Pending')
-
-    // const scheduledInterviews = userInterviews.filter ((interview) => 
-    //     interview.status = 'Scheduled')
-
-
-    useEffect(async () =>
-        dispatch(getAllUsersInterviewThunk()), [dispatch]
-    )
-    if (!user) {
-        history.push('/')
-        return null
+  let userProfile = {};
+  for (let keys in allUsers) {
+    console.log(allUsers[keys].id);
+    if (allUsers[keys].id == id) {
+      userProfile = allUsers[keys];
     }
+  }
+  console.log(userProfile, "------------user procfile");
+  console.log(allUsers, "these are all the users");
+  let user = useSelector((store) => store?.session["user"]);
+  interviews = Object.values(interviews);
+  console.log(interviews, "-----------------these are all the interviews");
+  // console.log(interviews, '--------------------all the interviews')
+  const userInterviews = interviews.filter(
+    (interview) => Number(interview.userId) == id
+  );
+  console.log(userInterviews, "--------------these belong to the users");
 
+  // console.log (userInterviews, '------------these belong th the uses')
 
-    if (userInterviews.length<1) {
-        return <>
-        <div className="no-user-int-cont">
-            <p> This user does not have any interviews</p>
-        </div>
-        </>
-    }
-    return (
+  // const pendingInterviews = userInterviews  .filter((interview) => interview.status === 'Pending')
 
+  // const scheduledInterviews = userInterviews.filter ((interview) =>
+  //     interview.status = 'Scheduled')
 
-        <>
-            <div className='allInt-container'>
-                {userInterviews.map((interview) => {
-                    // const interviewDate = new Date(interview.date)
-                    // const formattedDate = format(interviewDate, 'EEEE, MMMM d, yyyy', { timeZone: 'GMT' });
+  useEffect(async () => dispatch(getAllUsersInterviewThunk()), [dispatch]);
+  if (!user) {
+    history.push("/");
+    return null;
+  }
 
-                    return (
+//   if (userInterviews.length < 1) {
+//     return (
+//       <>
+//         <div className="no-user-int-cont">
+//           <p> This user does not have any interviews</p>
+//         </div>
+//       </>
+//     );
+//   }
+  return (
+    <>
+      <div className="single-user-main-cont">
+        <div className="single-usr-prfile-cont">
+          <div className="single-user-upper">
+            <img
+              src={
+                userProfile.image
+                  ? `https://jobshpere-profile-images.s3.amazonaws.com/${userProfile.image}`
+                  : userImage
+              }
+            ></img>
+            <p className="single-user-name">
+              {userProfile.firstName} {userProfile.last_name}
+            </p>
+            <p>{userProfile.location}</p>
+          </div>
+          <div className="single-user-btm">
+            <div>
 
-
-                        <div className='allInt-column'>
-                            <div className='allInt-top-cont'>
-                                <div className='allInt-com-letter'>
-                                    <p>{interview.company[0].toUpperCase()}</p>
-                                </div>
-                                <div>
-                                    <p className='allInt-com-pos'>{interview.position}</p>
-                                    <p className='allInt-com-name'>{interview.company}</p>
-                                </div>
-                            </div>
-                            <div className='allInt-btm-container'>
-                                <div className='allInt-btm-left-cont'>
-                                    <div className='allInt-loc-type-cont'>
-                                        <p className='allInt-loc-text'><FaLocationArrow className='allInt-loc-logo' />{interview.location}</p>
-                                        <p>
-                                            <MdWork className='allInt-type-logo' />{interview.type}
-                                        </p>
-                                    </div>
-
-                                    <div className='allInt-btm-bttns'>
-
-                                        {/* <button className='allInt-edit-btn'>
-                                        <OpenModalButton
-                                            buttonText={'Edit'}
-                                            
-                                            modalComponent={
-                                                <EditInterviewForm
-                                                    interview={interview} id={interview.id}
-                                                />
-                                            }
-                                        />
-                                    </button> */}
-
-
-
-
-
-                                        {/* <button className='allInt-del-btn'>
-    <OpenModalButton
-                                            buttonText={'Delete'}
-                                            modalComponent={
-                                                <DeleteInterview
-                                                    interview={interview}
-                                                />
-                                            }
-                                        />
-    </button>                                        */}
-
-                                        <NavLink exact to={`/interview/${interview.id}/detail`} ><button className='allInt-btns-more'>More</button></NavLink>
-
-
-                                    </div>
-
-
-                                </div>
-                                <div className='allInt-btm-right-container'>
-
-                                    {user.id === interview.userId && (
-
-                                        <p className='allInt-date'><BsCalendar2Week className='allInt-int-logo' />{interview['date'].slice(0, 17)}</p>
-                                    )}
-                                    {interview.status === 'Pending' && (
-                                        <p className='allInt-Pending'>{interview.status}</p>
-                                    )}
-                                    {interview.status === 'Scheduled' && (
-                                        <p className='allInt-Scheduled'>{interview.status}</p>
-                                    )}
-                                    {interview.status === 'Declined' && (
-                                        <p className='allInt-Declined'>{interview.status}</p>
-                                    )}
-                                </div>
-                            </div>
-
-
-
-
-
-                        </div>
-                    )
-                })}
+            <p className="single-user-career">{userProfile.career}</p>
             </div>
+            <p className="single-user-bio">{userProfile.bio}</p>
+            <p className="single-user-email">{userProfile.email}</p>
+          </div>
+        </div>
+      <div className="allInt-container">
+        {userInterviews.map((interview) => {
+          return (
+            <div className="allInt-column single">
+              <div className="allInt-top-cont">
+                <div className="allInt-com-letter">
+                  <p>{interview.company[0].toUpperCase()}</p>
+                </div>
+                <div>
+                  <p className="allInt-com-pos">{interview.position}</p>
+                  <p className="allInt-com-name">{interview.company}</p>
+                </div>
+              </div>
+              <div className="allInt-btm-container">
+                <div className="allInt-btm-left-cont">
+                  <div className="allInt-loc-type-cont">
+                    <p className="allInt-loc-text">
+                      <FaLocationArrow className="allInt-loc-logo" />
+                      {interview.location}
+                    </p>
+                    <p>
+                      <MdWork className="allInt-type-logo" />
+                      {interview.type}
+                    </p>
+                  </div>
 
-        </>
+                  <div className="allInt-btm-bttns">
+                    <NavLink exact to={`/interview/${interview.id}/detail`}>
+                      <button className="allInt-btns-more">More</button>
+                    </NavLink>
+                  </div>
+                </div>
+                <div className="allInt-btm-right-container">
+                  {user.id === interview.userId && (
+                    <p className="allInt-date">
+                      <BsCalendar2Week className="allInt-int-logo" />
+                      {interview["date"].slice(0, 17)}
+                    </p>
+                  )}
+                  {interview.status === "Pending" && (
+                    <p className="allInt-Pending">{interview.status}</p>
+                  )}
+                  {interview.status === "Scheduled" && (
+                    <p className="allInt-Scheduled">{interview.status}</p>
+                  )}
+                  {interview.status === "Declined" && (
+                    <p className="allInt-Declined">{interview.status}</p>
+                  )}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      </div>
+    </>
 
-        // <>
-        //     {scheduledInterviews.map((interview) => {
-        //         return (
-        //             <div key={interview.id}>
-        //                 <p>{interview.company}</p>
-        //                 <p>{interview.position}</p>
-        //                 <p>{interview.location}</p>
-        //                 <p>{interview.status}</p>
-        //                 <p>Helooooooooooooooooooo</p>
+    // <>
+    //     {scheduledInterviews.map((interview) => {
+    //         return (
+    //             <div key={interview.id}>
+    //                 <p>{interview.company}</p>
+    //                 <p>{interview.position}</p>
+    //                 <p>{interview.location}</p>
+    //                 <p>{interview.status}</p>
+    //                 <p>Helooooooooooooooooooo</p>
 
-        //             </div>
-        //         )
-        //     })}
+    //             </div>
+    //         )
+    //     })}
 
+    //     {pendingInterviews.map((interview) => {
+    //         return (
 
-        //     {pendingInterviews.map((interview) => {
-        //         return (
+    //         <div key={interview.id}>
+    //             <p>{interview.company}</p>
+    //             <p>{interview.position}</p>
+    //             <p>{interview.location}</p>
+    //             <p>{interview.status}</p>
+    //         </div>
+    //         )
+    //     })}
 
-        //         <div key={interview.id}>
-        //             <p>{interview.company}</p>
-        //             <p>{interview.position}</p>
-        //             <p>{interview.location}</p>
-        //             <p>{interview.status}</p>
-        //         </div>
-        //         )
-        //     })}
+    // </>
+    // <div>hello</div>
+  );
+};
 
-
-        // </>
-        // <div>hello</div>
-    )
-
-
-}
-
-export default SingleUser
-
-
+export default SingleUser;
